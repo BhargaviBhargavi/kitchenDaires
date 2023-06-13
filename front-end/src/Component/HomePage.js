951c0e5266601929de37b9de67c86a7f547009c9
Your Name <you@example.com>
2023-06-13 12:48:15 +0530

    feat:create the homePage

--- front-end/src/Component/HomePage.js
+++ front-end/src/Component/HomePage.js
@@ -6,13 +6,21 @@ function homepage()
         <div className="main">
             <div className="home">
                 <nav className="navbar">
-                    <img src="Resources/logo.png" alt="" className="image"/>
-                    <div className="recipes">Recipes</div>
-                    <div className="topics">Popular Meals</div>
-                    <div className="topics">Main Ingredients</div>
-                    <div className="topics">Healthy Food</div>
-                    <div className="topics">Occation</div>
-                    <div className="topics">Seasonal Food</div>
+                    <div className="firstContainer">
+                        <img src="Resources/logo.png" alt="" className="image"/>
+                        <input type="text" className="searchbar" placeholder="Search here"/>
+                        <img src="Resources/searchbar1.png" alt="" className="search"/>
+                        <img src="Resources/loginLogo.jpeg" alt="" className="loginlogo"/>
+                    </div>
+                    <div className="secondContainer">
+                        <div className="recipes">Recipes</div>
+                        <div className="topics">Popular Meals</div>
+                        <div className="topics">Main Ingredients</div>
+                        <div className="topics">Healthy Food</div>
+                        <div className="topics">Occation</div>
+                        <div className="topics">Seasonal Food</div>
+                        <div className="topics">About Us</div>
+                    </div>
                 </nav>
                 
             </div>
